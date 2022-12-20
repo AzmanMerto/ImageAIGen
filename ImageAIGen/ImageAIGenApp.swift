bd0ed245f190e1990a4a17479dac8c9ef9d9d9f0
NomoteteS <nomotetes.onetrue@icloud.com>
2022-12-20 14:12:29 +0200

    Initial Commit

---
+++ ImageAIGen/ImageAIGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageAIGenApp.swift
+//  ImageAIGen
+//
+//  Created by NomoteteS on 20.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ImageAIGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
